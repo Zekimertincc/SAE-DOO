4ee12bccee3aebf63fa1e2181bcb51d50a06151a
antoine-chen <antoinecoc2408@gmail.com>
2025-05-12 16:11:23 +0200

    16H11

---
+++ src/main/java/fr/iut/groupe/terraria/demo/Main.java
@@ -0,0 +1,30 @@
+package fr.iut.groupe.terraria.demo;
+
+import fr.iut.groupe.terraria.demo.controller.ControleurJeu;
+import fr.iut.groupe.terraria.demo.modele.Joueur;
+import fr.iut.groupe.terraria.demo.vue.VueJeu;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        VueJeu vue = new VueJeu();
+        Joueur joueur = new Joueur(100, 260);
+        ControleurJeu controleur = new ControleurJeu(vue, joueur);
+
+        Scene scene = new Scene(vue);
+        controleur.demarrer(scene);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Jeu MVC - Déplacement + Saut");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
