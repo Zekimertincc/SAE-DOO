c2d3358204f40fac66249d368346ead7bb1855cd
Enes <enes.erdogan@protonmail.com>
2025-05-15 10:56:44 +0200

    Modification classe Monde

--- src/main/java/fr/iut/groupe/terraria/demo/modele/monde/Monde.java
+++ src/main/java/fr/iut/groupe/terraria/demo/modele/monde/Monde.java
@@ -2,9 +2,9 @@ package fr.iut.groupe.terraria.demo.modele.monde;
 
 import fr.iut.groupe.terraria.demo.modele.personnage.Joueur;
 import fr.iut.groupe.terraria.demo.modele.personnage.ennemi.Ennemi;
-import fr.iut.groupe.terraria.demo.modele.personnage.Personnage;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Monde {
     private ArrayList<Ennemi> ennemis;
@@ -13,31 +13,57 @@ public class Monde {
         ennemis = new ArrayList<>();
     }
 
+    // Ajouter un ennemi
     public void ajouterEnnemi(Ennemi ennemi) {
         ennemis.add(ennemi);
     }
 
+    // Supprimer un ennemi
+    public void supprimerEnnemi(Ennemi ennemi) {
+        ennemis.remove(ennemi);
+    }
 
-    /*
-     boucle qui gere les ennemis et leurs comportement
-     le premier if c'est pour voir si le personnage est dans la zone des ennemis
-     si oui alors il va se deplacer vers le personnage
-     et si il est assez proche du personnage il le met des degats
-    */
+    // Vérifie si tous les ennemis sont morts
+    public boolean tousLesEnnemisSontMorts() {
+        for (Ennemi e : ennemis) {
+            if (!e.estMort()) return false;
+        }
+        return true;
+    }
+
+    // Mettre à jour les ennemis
     public void mettreAJour(Joueur joueur) {
-        for (Ennemi ennemi : ennemis) {
+        Iterator<Ennemi> iterator = ennemis.iterator();
+        while (iterator.hasNext()) {
+            Ennemi ennemi = iterator.next();
+
+            if (ennemi.estMort()) {
+                iterator.remove(); // Supprime l'ennemi mort
+                continue;
+            }
+
             if (ennemi.estDansZone(ennemi.getX() - 50, ennemi.getX() + 50)) {
                 if (ennemi.estProcheDe(joueur, 100)) {
-                    ennemi.seDeplacerVers(joueur);
+                    ennemi.seDeplacerVers(joueur); // Rapprochement
                 }
                 if (ennemi.estProcheDe(joueur, 10)) {
-                    ennemi.attaquer(joueur);
+                    ennemi.attaquer(joueur); // Attaque
                 }
             }
         }
     }
 
+    // Retourne la liste des ennemis
     public ArrayList<Ennemi> getEnnemis() {
         return ennemis;
     }
+
+    // Afficher les ennemis (console)
+    public void afficherEnnemis() {
+        for (Ennemi e : ennemis) {
+            System.out.println("Ennemi : x=" + e.getX() + " y=" + e.getY() + " PV=" + e.getVie());
+        }
+    }
+
 }
+
