c8f35371344d37cc195ba754f2ff642d6d9af52e
zekimertincc <zekimertinc@gmail.com>
2025-05-27 11:10:17 +0200

    derniere version de inventaire avec l inventaire qui montre quantite

--- src/main/java/fr/iut/groupe/terraria/demo/controller/InventaireController.java
+++ src/main/java/fr/iut/groupe/terraria/demo/controller/InventaireController.java
@@ -2,6 +2,7 @@ package fr.iut.groupe.terraria.demo.controller;
 
 import fr.iut.groupe.terraria.demo.modele.Inventaire;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -30,14 +31,24 @@ public class InventaireController {
 
             StackPane slot = new StackPane();
             slot.setPrefSize(48, 48);
-            slot.setStyle("-fx-border-color: white; -fx-background-color: #5a5a5a;");
+            slot.setStyle("-fx-border-color: white; -fx-background-color: #3a3a3a;");
 
             Label label = new Label(nom + " x" + quantite);
-            label.setStyle("-fx-text-fill: white;");
-            slot.getChildren().add(label);
+            label.setStyle("-fx-text-fill: white; -fx-font-size: 10px;");
+            StackPane.setAlignment(label, Pos.CENTER);
 
+            slot.getChildren().add(label);
             slotBar.getChildren().add(slot);
             i++;
         }
+
+        // Geri kalan boş slotları göster
+        for (; i < 9; i++) {
+            StackPane emptySlot = new StackPane();
+            emptySlot.setPrefSize(48, 48);
+            emptySlot.setStyle("-fx-border-color: gray; -fx-background-color: #222;");
+            slotBar.getChildren().add(emptySlot);
+        }
     }
+
 }
