08a96fd73ad59057830e43a9e26b796e7f5ffc3d
Enes <enes.erdogan@protonmail.com>
2025-06-05 09:56:36 +0200

    Correction de VueJeu : placement du joueur, binding et ordre d'affichage

--- src/main/java/fr/iut/groupe/terraria/demo/vue/VueJeu.java
+++ src/main/java/fr/iut/groupe/terraria/demo/vue/VueJeu.java
@@ -1,9 +1,11 @@
 package fr.iut.groupe.terraria.demo.vue;
 
+import fr.iut.groupe.terraria.demo.modele.personnage.EtatTemporaire;
+import fr.iut.groupe.terraria.demo.modele.personnage.Joueur;
 import fr.iut.groupe.terraria.demo.modele.ressource.Arbre;
-import fr.iut.groupe.terraria.demo.modele.ressource.Roche;
-import fr.iut.groupe.terraria.demo.modele.ressource.Ressource;
 import fr.iut.groupe.terraria.demo.modele.ressource.CanneSucre;
+import fr.iut.groupe.terraria.demo.modele.ressource.Ressource;
+import fr.iut.groupe.terraria.demo.modele.ressource.Roche;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -15,28 +17,38 @@ public class VueJeu extends Pane {
     private final VueMap vueMap;
     private final VueJoueur vueJoueur;
     private final List<Ressource> ressources;
+    private final Joueur joueur;
 
     public VueJeu() {
         this.setPrefSize(640, 383);
 
         vueMap = new VueMap();
-        vueJoueur = new VueJoueur();
         ressources = new ArrayList<>();
+        int[][] mapCollision = vueMap.getCollisionMap();
 
-        // Ajout des arbres
+        // ✅ Position plus visible pour le joueur
+        joueur = new Joueur(100, 200, 100, new EtatTemporaire(), mapCollision);
+
+        vueJoueur = new VueJoueur();
+        vueJoueur.setJoueur(joueur); // binding unique
+
+        // Ajout de la map d'abord
+        this.getChildren().add(vueMap.getTilePane());
+
+        // ➕ Arbres
         ressources.add(new Arbre(300, 200));
         ressources.add(new Arbre(500, 220));
         ressources.add(new Arbre(150, 250));
 
-        // Ajout des rochers
+        // ➕ Roches
         ressources.add(new Roche(400, 300));
         ressources.add(new Roche(600, 350));
 
+        // ➕ Canne à sucre
         ressources.add(new CanneSucre(200, 250));
         ressources.add(new CanneSucre(500, 280));
 
-
-        // Affichage des ressources
+        // ➕ Affichage des ressources
         for (Ressource r : ressources) {
             Image img;
             if (r instanceof Arbre) {
@@ -44,11 +56,10 @@ public class VueJeu extends Pane {
             } else if (r instanceof Roche) {
                 img = new Image(getClass().getResourceAsStream("/fr/iut/groupe/terraria/demo/roche.png"));
             } else if (r instanceof CanneSucre) {
-                img = new Image(getClass().getResourceAsStream("/fr/iut/groupe/terraria/demo/CanneSucre.png"));  // change k3.png si besoin
+                img = new Image(getClass().getResourceAsStream("/fr/iut/groupe/terraria/demo/CanneSucre.png"));
             } else {
                 continue;
-
-        }
+            }
 
             ImageView imgView = new ImageView(img);
             if (r instanceof Roche) {
@@ -65,11 +76,8 @@ public class VueJeu extends Pane {
             this.getChildren().add(imgView);
         }
 
-        // Ajout map et joueur
-        this.getChildren().addAll(
-                vueMap.getTilePane(),
-                vueJoueur.getJoueurVue()
-        );
+        // ✅ Ajout du joueur après tout (il est au-dessus)
+        this.getChildren().add(vueJoueur.getJoueurVue());
     }
 
     public int[][] getCollisionMap() {
@@ -82,5 +90,9 @@ public class VueJeu extends Pane {
 
     public List<Ressource> getRessources() {
         return ressources;
+    }
+
+    public Joueur getJoueur() {
+        return joueur;
     }
 }
