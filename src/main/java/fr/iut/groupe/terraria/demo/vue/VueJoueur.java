5715b0bd96e3b91ba028455cc5574aa87cbb46e6
Enes <enes.erdogan@protonmail.com>
2025-06-05 09:56:28 +0200

    Ajout du binding layoutX/layoutY et de l'animation dans VueJoueur

--- src/main/java/fr/iut/groupe/terraria/demo/vue/VueJoueur.java
+++ src/main/java/fr/iut/groupe/terraria/demo/vue/VueJoueur.java
@@ -1,5 +1,6 @@
 package fr.iut.groupe.terraria.demo.vue;
 
+import fr.iut.groupe.terraria.demo.modele.personnage.Joueur;
 import javafx.animation.AnimationTimer;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -9,16 +10,17 @@ public class VueJoueur {
     private final Image[] idleFrames;
     private final Image[] runFrames;
     private int frameIndex = 0;
-    private long lastTime  = 0;
+    private long lastTime = 0;
     private String currentState = "idle";
     private boolean lookingRight = true;
 
     public VueJoueur() {
-        idleFrames = new Image[]{
+        // Chargement des frames
+        idleFrames = new Image[] {
                 new Image(getClass().getResourceAsStream("/fr/iut/groupe/terraria/demo/i1.png")),
                 new Image(getClass().getResourceAsStream("/fr/iut/groupe/terraria/demo/i2.png"))
         };
-        runFrames = new Image[]{
+        runFrames = new Image[] {
                 new Image(getClass().getResourceAsStream("/fr/iut/groupe/terraria/demo/k1.png")),
                 new Image(getClass().getResourceAsStream("/fr/iut/groupe/terraria/demo/k2.png")),
                 new Image(getClass().getResourceAsStream("/fr/iut/groupe/terraria/demo/k3.png")),
@@ -33,30 +35,47 @@ public class VueJoueur {
         joueurVue.setFitWidth(64);
         joueurVue.setFitHeight(96);
 
+        // Animation frame par frame
         new AnimationTimer() {
             @Override
             public void handle(long now) {
                 if (now - lastTime > 120_000_000) {
-                    frameIndex = (frameIndex + 1) %
-                            (currentState.equals("idle") ? idleFrames.length : runFrames.length);
-                    joueurVue.setImage(currentState.equals("idle")
-                            ? idleFrames[frameIndex]
-                            : runFrames[frameIndex]);
+                    frameIndex = (frameIndex + 1) % (
+                            currentState.equals("idle") ? idleFrames.length : runFrames.length
+                    );
+                    joueurVue.setImage(
+                            currentState.equals("idle") ? idleFrames[frameIndex] : runFrames[frameIndex]
+                    );
                     lastTime = now;
                 }
             }
         }.start();
     }
 
-
+    /**
+     * Met à jour le sprite affiché selon l'état ("idle" ou "run") et la direction.
+     */
     public void updateSprite(String state, boolean isRight) {
-        if (!state.equals(currentState)) frameIndex = 0;
+        if (!state.equals(currentState)) {
+            frameIndex = 0;
+        }
         currentState = state;
+
         if (isRight != lookingRight) {
             joueurVue.setScaleX(isRight ? 1 : -1);
             lookingRight = isRight;
         }
     }
 
-    public ImageView getJoueurVue() { return joueurVue; }
+    /**
+     * Lie le joueur modèle à la vue. Appelé une seule fois.
+     */
+    public void setJoueur(Joueur joueur) {
+        joueurVue.layoutXProperty().bind(joueur.xProperty());
+        joueurVue.layoutYProperty().bind(joueur.yProperty());
+    }
+
+    public ImageView getJoueurVue() {
+        return joueurVue;
+    }
 }
