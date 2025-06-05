697d0e160aff353d417d80ddacb1027d9ce436e2
Enes <enes.erdogan@protonmail.com>
2025-06-05 09:54:27 +0200

    Refactor de la classe Personnage : ajout de DoubleProperty pour x et y

--- src/main/java/fr/iut/groupe/terraria/demo/modele/personnage/Personnage.java
+++ src/main/java/fr/iut/groupe/terraria/demo/modele/personnage/Personnage.java
@@ -1,15 +1,19 @@
 package fr.iut.groupe.terraria.demo.modele.personnage;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
 public abstract class Personnage {
-    protected double x, y;
+    protected DoubleProperty x = new SimpleDoubleProperty();
+    protected DoubleProperty y = new SimpleDoubleProperty();
     protected double vitesseY;
     protected double vitesseX;
     protected int vie, vieMax;
     protected int degats;
 
     public Personnage(double x, double y, double vitesseX, double vitesseY, int vie, int vieMax, int degats) {
-        this.x = x;
-        this.y = y;
+        this.x.set(x);
+        this.y.set(y);
         this.vitesseX = vitesseX;
         this.vitesseY = vitesseY;
         this.vie = vie;
@@ -18,36 +22,35 @@ public abstract class Personnage {
     }
 
     /*
-        La fonction prend en parametre le nombre de vie, et l'ajoute sur le personnage
-        Si la vie depasse 5 coeurs, alors le perso à 5 coeur, la vie est en dessous 0 alors le personnage a 0 coeur
+        La fonction prend en paramètre le nombre de vie, et l'ajoute sur le personnage.
+        Si la vie dépasse 5 coeurs, alors le perso a 5 coeurs. Si la vie est en dessous de 0, alors il a 0 coeur.
     */
     public void gagnerVie(int quantite) {
         vie += quantite;
-        if (vie > vieMax) {
-            vie = vieMax;
-        }
-        if (vie < 0) {
-            vie = 0;
-        }
+        if (vie > vieMax) vie = vieMax;
+        if (vie < 0) vie = 0;
     }
-    // utilise la fonction gagner de la vie mais envoie un nombre negatif pour faire perdre de la vie au joueur
+
+    // utilise la fonction gagner de la vie mais envoie un nombre négatif pour faire perdre de la vie au joueur
     public void subirDegats(int degats) {
         gagnerVie(-degats);
     }
 
-    // verifie si le personnage est mort
+    // vérifie si le personnage est mort
     public boolean estMort() {
         return vie <= 0;
     }
 
-    public int getVie() {
-        return vie;
-    }
-    public int getVieMax() {
-        return vieMax;
-    }
-    public double getX() { return x; }
-    public double getY() { return y; }
+    public int getVie() { return vie; }
+    public int getVieMax() { return vieMax; }
+
+    public double getX() { return x.get(); }
+    public void setX(double x) { this.x.set(x); }
+    public DoubleProperty xProperty() { return x; }
+
+    public double getY() { return y.get(); }
+    public void setY(double y) { this.y.set(y); }
+    public DoubleProperty yProperty() { return y; }
 
     public void setVitesseX(double vitesseX) {
         this.vitesseX = vitesseX;
