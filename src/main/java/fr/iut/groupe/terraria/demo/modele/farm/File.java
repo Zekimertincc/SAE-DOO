ad8864c3223d77e7ea34e3fe530f7b563227eb48
Enesss00 <enes.erdogan@protonmail.com>
2025-06-17 23:27:26 +0200

    Integrate main branch model classes

---
+++ src/main/java/fr/iut/groupe/terraria/demo/modele/farm/File.java
@@ -0,0 +1,11 @@
+package fr.iut.groupe.terraria.demo.modele.farm;
+
+import fr.iut.groupe.terraria.demo.modele.item.Item;
+
+public class File extends Farm{
+
+    public File() {
+        super("File");
+    }
+
+}
---
+++ src/main/java/fr/iut/groupe/terraria/demo/modele/personnage/ennemi/Aigle.java
@@ -0,0 +1,22 @@
+package fr.iut.groupe.terraria.demo.modele.personnage.ennemi;
+
+import fr.iut.groupe.terraria.demo.modele.personnage.Joueur;
+
+public class Aigle extends Ennemi {
+    public Aigle(double x, double y) {
+        super(x, y, 1.5, 0, 20, 2, 100);
+    }
+
+    // quand le loup a moins de 25% de ses hp max il lache un pique qui met la vie actuelle du joueur à 1 coeur puis meurt.
+    @Override
+    public void comportement(Joueur joueur) {
+        if (this.getVie() < this.getVieMax() / 4) {
+            joueur.mettreAPV(1);
+            vieProperty().set(0);
+        }
+    }
+    @Override
+    public String getNom() {
+        return "Aigle";
+    }
+}
